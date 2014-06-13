6878af426e790acd21e5ec5661d2ab2eb9f2de3c
Shuhao Wu <shuhao@shuhaowu.com>
2014-06-13 14:31:58 -0400

    Added documentations.

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
 gohttpmock
 ==========
 
-Mocking HTTP calls in go tests. Set mock responses and what not.
+Mocking HTTP calls in Go. 
+
+Examples
+--------
+
+Here's a simple use case, with test written with [gocheck][gc].
+
+[gc]: http://labix.org/gocheck
+
+    func (*Suite) TestRequest(c *C) {
+        // Overrides http.DefaultTransport with a recording transport.
+        record := gohttpmock.StartTestHTTPCall()
+        // Makes sure that we clean up and don't pollute state with other
+        // tests. Also restores http.DefaultTransport
+        defer gohttpmock.EndTestHTTPCall()
+
+        // Defining an URL.
+        record.When("POST", "http://example.com/").Respond(200, "body", "text/plain")
+
+        resp, err := http.Post("http://example.com/", "request_body", "text/plain")
+
+        // no error
+        c.Assert(err, IsNil)
+
+        // Read the response body
+        buf := &bytes.Buffer{}
+        buf.ReadFrom(resp.Body)
+        respBody := buf.String()
+
+        // resp.Body reads to "body" as we defined.
+        c.Assert(respBody, Equals, "body")
+
+        // record.Requests contains a list of *http.Request in the order of
+        // which they are recorded. It currently has a length of 1.
+        c.Assert(record.Requests, HasLen, 1)
+
+        // All the information from the original request is obtained.
+        c.Assert(record.Requests[0].URL.String(), Equals, "http://example.com/")
+        c.Assert(record.Requests[0].Method, Equals, "POST")
+
+        // func RequestBody(i int) string is just a convenience function for 
+        // getting the request body to be a string, at index i
+        c.Assert(record.RequestBody(0), Equals, "request_body")
+    }
+
+You can also generate a response on demand:
+    
+    i := 0
+    f := func(req *http.Request) *gohttpmock.TestResponse {
+        i++
+        return gohttpmock.NewTestResponse(200, fmt.Sprintf("body %d", i), "text/plain")
+    }
+    record.When("POST", "http://example.com/").RespondFunc(f)
+
+    // body 1
+    resp, err = http.Post("http://example.com/", "request_body", "text/plain")
+    // body 2
+    resp, err = http.Post("http://example.com/", "request_body", "text/plain")
+
+If you do not define a route, any request while `StartTestHTTPCall` is called
+and `EndTestHTTPCall` is not will result in a 404.
+
+For more information, see the [documentation][doc].
+
+[doc]: http://godoc.org/github.com/shuhaowu/gohttpmock
--- gohttpmock.go
+++ gohttpmock.go
@@ -10,11 +10,17 @@ import (
 var defaultTransport = http.DefaultTransport
 
 type TestResponse struct {
+	// The status code of the response
 	StatusCode int
-	Body       string
-	Header     http.Header
+
+	// The body of the response in a string. It will be turned into a io.ReadCloser
+	Body string
+
+	// The headers of the response
+	Header http.Header
 }
 
+// Shortcut function to create a new TestResponse
 func NewTestResponse(status int, body, contentType string) *TestResponse {
 	return &TestResponse{
 		StatusCode: status,
@@ -25,7 +31,9 @@ func NewTestResponse(status int, body, contentType string) *TestResponse {
 
 type HandlerFunc func(*http.Request) *TestResponse
 
+// The transport that replaces http.DefaultTransport
 type RecordingTransport struct {
+	// A list of *http.Request that is in the order of which they are requested.
 	Requests []*http.Request
 
 	responses map[string]interface{}
@@ -60,34 +68,48 @@ func (t *RecordingTransport) RoundTrip(req *http.Request) (*http.Response, error
 	}, nil
 }
 
-func (t *RecordingTransport) RequestBody(index int) string {
-	if t.Requests[index].Body == nil {
+// Gets the _i_th request's body as a string. A shortcut to reading it
+// via a buffer or something.
+func (t *RecordingTransport) RequestBody(i int) string {
+	if t.Requests[i].Body == nil {
 		return ""
 	}
 
 	buf := bytes.Buffer{}
-	buf.ReadFrom(t.Requests[index].Body)
+	buf.ReadFrom(t.Requests[i].Body)
 	return buf.String()
 }
 
+// Creates an expectation. Usually used in shorthand like the following:
+//
+//   record.When(method, url).Respond(status, body, contentType)
+//
+// Using it on its own is not really that useful
 func (t *RecordingTransport) When(method, url string) *RequestHandler {
 	return &RequestHandler{method: method, url: url, recordingTransport: t}
 }
 
+// A temporary struct for a prettier syntax.
 type RequestHandler struct {
 	method             string
 	url                string
 	recordingTransport *RecordingTransport
 }
 
+// Define the response for a particular method/url.
 func (t *RequestHandler) Respond(status int, body string, contentType string) {
 	t.recordingTransport.responses[requestKey(t.method, t.url)] = NewTestResponse(status, body, contentType)
 }
 
+// Define the response for a particular method/url but with a function.
 func (t *RequestHandler) RespondFunc(f HandlerFunc) {
 	t.recordingTransport.responses[requestKey(t.method, t.url)] = f
 }
 
+// Starts the test http call process. Disallow any real http call and record
+// all of them.
+//
+// Calling this should follow with a defer gohttpmock.EndTestHTTPCall()
 func StartTestHTTPCall() *RecordingTransport {
 	t := &RecordingTransport{}
 	t.responses = make(map[string]interface{})
@@ -95,6 +117,7 @@ func StartTestHTTPCall() *RecordingTransport {
 	return t
 }
 
+// Restores the http.DefaultTransport, allowing real http calls again.
 func EndTestHTTPCall() {
 	http.DefaultTransport = defaultTransport
 }
